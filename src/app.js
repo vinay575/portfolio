fd37143e64c7607ccc2b55ebc692f05b68f350fc
vinay <bhaskarvinaychitturi@gmail.com>
2023-12-06 13:45:33 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Data from './components/Data';
-import Cursor from './components/cursor';
+import Cursor from './components/cursorBg';
+import Data from './components/Home';
+import  About from './components/AboutPg';
+import  Skills from './components/Skills';
+import  Star from './components/Stars';
+
 
 const App = () => {
   return (
     <div>
       <Cursor/>
       <Data/>
+      <About/>
+      <Skills/>
+      
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+const Data = () => {
+  return (
+<section id="app">
+    <section className="banner">
+        <header className="header">
+          <a href="#" className="logo">
+            <span>My</span>Portfolio_
+          </a>
+          <ul>
+            <li><a id="one" href="#Home">Home</a></li>
+            <li><a id="two" href="#Portfolio">Portfolio</a></li>
+            <li><a id="three" href="#About">About</a></li>
+            <li><a id="four" href="#ContactMe">Contact</a></li>
+          </ul>
+        </header>
+        <section>
+          <div className="content">
+            <h3>Hey_</h3>
+            <h2>I'm <span id="text">Vinay Bhaskar</span></h2>
+            <p>
+              Hello there! 👋 I'm Vinay, a passionate and dynamic individual diving headfirst into the world
+              of web development.<br></br>As a recent graduate and aspiring full-stack developer, I bring fresh
+              perspectives and a hunger for learning to every project.
+            </p>
+          </div>
+        </section>
+      </section>
+</section>
+  )
+}
+
+export default Data;
