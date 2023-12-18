afe214d312fa116db8e1a06881b74431d59a6074
vinay <bhaskarvinaychitturi@gmail.com>
2023-12-18 15:36:41 +0530

    update

--- src/app.js
+++ src/app.js
@@ -5,15 +5,21 @@ import Data from './components/Home';
 import  About from './components/AboutPg';
 import  Skills from './components/Skills';
 import  Star from './components/Stars';
-
+import  Contact from './components/contact';
+// <Cursor/>
+//<Data/>
+//<About/>
+//<Skills/>  
 
 const App = () => {
   return (
     <div>
-      <Cursor/>
-      <Data/>
-      <About/>
-      <Skills/>  
+    <Cursor/>
+    <Data/>
+    <About/>
+    <Skills/>  
+    <Contact/>
+     
     </div>
   );
 }
